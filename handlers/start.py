9a508c710ae28d806a89ed30b228657b0f8be39e
cancinconnnbot <81495310+cancinconnnbot@users.noreply.github.com>
2021-05-05 17:31:29 +0300

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,42 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+
+
+
+
+@Client.on_message(filters.command("start") & filters.private & ~filters.channel)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""Hello 👋 there! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📜 User Manual 📜\' button below to know how you can use me.\n\n🔴 The Assistant must be in your group to play music in the voice chat of your group.\n\n🔴 More info & commands mentioned in the [User Manual](https://telegra.ph/Serenity-Music-Bot-05-05)\n\nA project by @SerenityBots""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "📜 User Manual 📜", url="https://telegra.ph/Serenity-Music-Bot-05-05")
+                  ],[
+                    InlineKeyboardButton(
+                        "👨‍💻 Updates 👨‍💻", url="https://t.me/SerenityBots"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "Support Chat 🎙️", url="https://t.me/SerenitySupport"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**🔴 Music player is online**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🎙️ Support Group 🎙️", url="https://t.me/SerenitySupport")
+                ]
+            ]
+        )
+   )
